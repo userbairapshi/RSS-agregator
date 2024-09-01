47d293550af5ffa3b12e72ce1b9f324d554324d5
Баира Пшихачева <bairapsihaceva@192.168.1.4>
2024-09-01 19:44:20 +0300

    add modal window

--- src/view.js
+++ src/view.js
@@ -1,76 +1,136 @@
+/* global bootstrap */
+
 import onChange from 'on-change';
 import i18next from 'i18next';
 
 export default (state) => {
-    const formElement = document.querySelector('form');
-    const inputElement = document.querySelector('input[name="url"]');
-    const feedbackElement = document.querySelector('.feedback');
-    const feedsContainer = document.querySelector('.feeds');
-    const postsContainer = document.querySelector('.posts');
+  const formElement = document.querySelector('form');
+  const inputElement = document.querySelector('input[name="url"]');
+  const feedbackElement = document.querySelector('.feedback');
+  const feedsContainer = document.querySelector('.feeds');
+  const postsContainer = document.querySelector('.posts');
+  const modalElement = document.getElementById('modal');
+  const modalTitle = modalElement.querySelector('.modal-title');
+  const modalBody = modalElement.querySelector('.modal-body');
+  const fullArticleLink = modalElement.querySelector('.full-article');
 
-    const renderError = (error) => {
-        if (error) {
-            inputElement.classList.add('is-invalid');
-            feedbackElement.textContent = error;
-            feedbackElement.classList.remove('success');
-        } else {
-            inputElement.classList.remove('is-invalid');
-            feedbackElement.textContent = i18next.t('success');
-            feedbackElement.classList.add('success');
-        }
-    };
+  const renderError = (error) => {
+    if (error) {
+      inputElement.classList.add('is-invalid');
+      feedbackElement.textContent = error;
+      feedbackElement.classList.remove('success');
+    } else {
+      inputElement.classList.remove('is-invalid');
+      feedbackElement.textContent = i18next.t('success');
+      feedbackElement.classList.add('success');
+    }
+  };
 
-    const renderFormValid = () => {
-        if (state.form.isValid) {
-            inputElement.value = '';
-            feedbackElement.textContent = i18next.t('success');
-            inputElement.focus();
-            formElement.reset();
-        }
-    };
+  const renderFormValid = () => {
+    if (state.form.isValid) {
+      inputElement.value = '';
+      feedbackElement.textContent = i18next.t('success');
+      inputElement.focus();
+      formElement.reset();
+    }
+  };
 
-    const renderFeeds = () => {
-        feedsContainer.innerHTML = '';
-        const feedList = document.createElement('ul');
-        state.feeds.forEach((feed) => {
-            const feedItem = document.createElement('li');
-            const feedTitle = document.createElement('h3');
-            const feedDescription = document.createElement('p');
-            feedTitle.textContent = feed.title;
-            feedDescription.textContent = feed.description;
-            feedItem.append(feedTitle, feedDescription);
-            feedList.appendChild(feedItem);
-        });
-        feedsContainer.appendChild(feedList);
-    };
+  const renderFeeds = () => {
+    feedsContainer.innerHTML = '';
+    const feedDivTitle = document.createElement('div');
+    const feedDivTitleH2 = document.createElement('h2');
+    const feedList = document.createElement('ul');
 
-    const renderPosts = () => {
-        postsContainer.innerHTML = '';
-        const postList = document.createElement('ul');
-        state.posts.forEach((post) => {
-            const postItem = document.createElement('li');
-            const postLink = document.createElement('a');
-            postLink.setAttribute('href', post.link);
-            postLink.setAttribute('target', '_blank');
-            postLink.textContent = post.title;
-            postItem.appendChild(postLink);
-            postList.appendChild(postItem);
-        });
-        postsContainer.appendChild(postList);
-    };
+    feedDivTitle.classList.add('card-body', 'card', 'border-0');
+    feedDivTitleH2.classList.add('card-title', 'h4');
 
-    return onChange(state, (path, value) => {
-        if (path === 'form.error') {
-            renderError(value);
-        }
-        if (path === 'form.isValid') {
-            renderFormValid();
-        }
-        if (path === 'feeds') {
-            renderFeeds();
-        }
-        if (path === 'posts') {
-            renderPosts();
-        }
+    feedDivTitleH2.textContent = 'Фиды';
+    feedDivTitle.appendChild(feedDivTitleH2);
+    state.feeds.forEach((feed) => {
+      const feedItem = document.createElement('li');
+      const feedTitle = document.createElement('h3');
+      const feedDescription = document.createElement('p');
+
+      feedTitle.classList.add('h6', 'm-0', 'fw-bold');
+
+      feedTitle.textContent = feed.title;
+      feedDescription.textContent = feed.description;
+      feedItem.append(feedTitle, feedDescription);
+      feedList.appendChild(feedItem);
     });
+    feedsContainer.appendChild(feedDivTitle);
+    feedsContainer.appendChild(feedList);
+  };
+
+  const renderPosts = () => {
+    postsContainer.innerHTML = '';
+    const postDivTitle = document.createElement('div');
+    const postTitle = document.createElement('h2');
+    const postList = document.createElement('ul');
+    postDivTitle.appendChild(postTitle);
+    postsContainer.appendChild(postDivTitle);
+
+    postDivTitle.classList.add('card-body', 'fw-bold');
+    postTitle.classList.add('card-title', 'h4');
+
+    postTitle.textContent = 'Посты';
+    state.posts.forEach((post) => {
+      const postItem = document.createElement('li');
+      const postLink = document.createElement('a');
+      const postButton = document.createElement('button');
+
+      postItem.classList.add('item-list');
+
+      if (post.isRead) {
+        postLink.classList.add('fw-normal');
+      } else {
+        postLink.classList.add('fw-bold');
+      }
+
+      postButton.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+
+      postLink.setAttribute('href', post.link);
+      postLink.setAttribute('target', '_blank');
+      postLink.textContent = post.title;
+      postButton.textContent = 'Просмотр';
+
+      postButton.addEventListener('click', () => {
+        post.isRead = true;
+        renderPosts();
+        showModal(post);
+      });
+
+      postItem.append(postLink, postButton);
+      postList.appendChild(postItem);
+    });
+    postsContainer.appendChild(postList);
+    
+  };
+
+  const showModal = (post) => {
+    modalTitle.textContent = post.title;
+    modalBody.textContent = post.description;
+    fullArticleLink.href = post.link;
+
+    const modal = new bootstrap.Modal(modalElement);
+    modal.show();
+  };
+
+  return onChange(state, (path, value) => {
+    if (path === 'form.error') {
+      renderError(value);
+    }
+    if (path === 'form.isValid') {
+      renderFormValid();
+    }
+    if (path === 'feeds') {
+      renderFeeds();
+    }
+    if (path === 'posts') {
+      renderPosts();
+    }
+    if (path.startsWith('posts') && path.endsWith('.isRead')) {
+      renderPosts();
+    }
+  });
 };
