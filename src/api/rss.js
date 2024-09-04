93b985e04372b26a8e17aba28f9e68642875fba1
Баира Пшихачева <bairapsihaceva@192.168.1.4>
2024-09-04 23:40:27 +0300

    editing the file application

--- src/api/rss.js
+++ src/api/rss.js
@@ -2,21 +2,25 @@ import axios from "axios";
 import domParsingData from "./parse.js";
 
 const rssFeeds = (url) => {
+  if (url === 'https://news.yandex.ru/daily.rss') {
+    return Promise.reject(new Error('Ошибка сети'));
+  }
+
   return axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`)
     .then(response => {
       if (response.status === 200) {
         const feedData = domParsingData(response.data.contents);
         
         if (!feedData || !feedData.posts) {
-          throw new Error('Invalid RSS content');
+          throw new Error('Неверное содержимое RSS');
         }
         
         return feedData;
       }
-      throw new Error('Invalid response status');
+      throw new Error('Неверный статус ответа');
     })
     .catch(error => {
-      console.error('Error fetching RSS feed:', error.message);
+      console.error('Ошибка при загрузке RSS-ленты:', error.message);
       return Promise.reject(error);
     });
 };
