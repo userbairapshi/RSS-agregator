bb9a3476b0ceb7b457ea08e1d023718e82a98661
Баира Пшихачева <bairapsihaceva@192.168.1.4>
2024-09-05 22:05:46 +0300

    add 'Ошибка сети'

--- src/api/rss.js
+++ src/api/rss.js
@@ -2,13 +2,10 @@ import axios from 'axios';
 import domParsingData from './parse.js';
 
 const rssFeeds = (url) => {
-  return axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`, {
-    timeout: 10000,
-  })
+  return axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}&disableCache=true`)
     .then(response => {
       if (response.status === 200) {
         const feedData = domParsingData(response.data.contents);
-        
         if (!feedData || !feedData.posts) {
           throw new Error('Ресурс не содержит валидный RSS');
         }
@@ -16,14 +13,14 @@ const rssFeeds = (url) => {
       }
       throw new Error('Ресурс не содержит валидный RSS');
     })
-    .catch((error) => {
-      if (error.code === 'ECONNABORTED' || error.message === 'Network Error') {
-        throw new Error('Ошибка сети');
-      } else if (error.response && (error.response.status === 404 || error.response.status === 500)) {
+    .catch(error => {
+      if (error.request) {
+        console.error('Ошибка сети: не удалось загрузить ресурс');
         throw new Error('Ошибка сети');
       } else {
-        throw new Error('Ресурс не содержит валидный RSS');
+        console.error('Ошибка:', error.message);
       }
+      throw new Error('Ресурс не содержит валидный RSS');
     });
 };
 
