d779e8ec66190cc6544a759514ff4f6819682356
Баира Пшихачева <bairapsihaceva@192.168.1.4>
2024-09-08 21:17:05 +0300

    file eslintrc.cjc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,26 +1,25 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: [
-      'airbnb-base',
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'always',
+        jsx: 'always',
+      },
     ],
-    parserOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-    },
-    rules: {
-      'import/extensions': [
-        'error',
-        'ignorePackages',
-        {
-          js: 'always',
-          jsx: 'always',
-        },
-      ],
-      'no-console': 'off',
-      'prefer-destructuring': 'off',
-    },
-  };
-  
+    'no-console': 'off',
+    'prefer-destructuring': 'off',
+  },
+};
